ae22fbaf823776b820a8195f5976f14fb93d1a02
ppastorf <pdrpfernandes99@gmail.com>
2023-11-01 17:55:42 -0300

    refactor generator

---
+++ go-generator/device.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+	"context"
+	"sync"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Message %s published on topic %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectionLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connection Lost: %s\n", err.Error())
+}
+
+type Device struct {
+	DeviceID string
+	Topic string
+	PublishTimeout time.Duration
+	mqttClient mqtt.Client
+}
+
+func NewDevice(deviceID string, topic string, publishTimeout time.Duration, mqttOptions *mqtt.ClientOptions) (*Device, error) {
+	clientID := fmt.Sprintf("%s DeviceID: %s", time.Now().String(), deviceID)
+		
+	mqttOptions.SetClientID(clientID)
+	mqttOptions.SetDefaultPublishHandler(messagePubHandler)
+	mqttOptions.OnConnect = connectHandler
+	mqttOptions.OnConnectionLost = connectionLostHandler
+
+	client := mqtt.NewClient(mqttOptions)
+	token := client.Connect()
+	if token.Wait() && token.Error() != nil {
+		return nil, fmt.Errorf("Failed to connect mqtt client: %s", token.Error())
+	}
+
+	// token = client.Subscribe(topic, 1, nil)
+	// token.Wait()
+	// fmt.Printf("Subscribed to topic %s\n", topic)
+
+	return &Device{
+		DeviceID: deviceID,
+		Topic: topic,
+		PublishTimeout: publishTimeout,
+		mqttClient: client,
+	}, nil
+}
+
+func (d *Device) generateMsgPayload() string {
+	date := time.Now().Format("01/02/2006 15:04:05")
+	agentId := fmt.Sprintf("AGENT%s", d.DeviceID)
+	temperature := rand.Intn(60)
+	moisture := rand.Intn(60)
+	state := "ON"
+
+	return fmt.Sprintf(`{ "date":"%s","agent_id":"%s","temperature":%d,"moisture":%d,"state":"%s" }`, date, agentId, temperature, moisture, state)
+}
+
+func (d *Device) PublishData(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer d.mqttClient.Disconnect(100)
+
+	fmt.Printf("Started publishing messages for device %s\n", d.DeviceID)
+
+	for {
+		select{
+		case <-ctx.Done():
+			fmt.Printf("Done publishing messages for device %s\n", d.DeviceID)
+			return
+		default:
+			payload := d.generateMsgPayload()
+			token := d.mqttClient.Publish(d.Topic, 0, false, payload)
+			token.Wait()
+			time.Sleep(d.PublishTimeout)
+		}
+	}
+
+}
---
+++ go-generator/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+	"strconv"
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	brokerAddr 			:= ReadEnvVariable("GENERATOR_BROKER_ADDRESS", "localhost:1883")
+	topic		 		:= ReadEnvVariable("GENERATOR_TOPIC", "farm_id/device_id/testclient1")
+	
+	numDevicesStr 		:= ReadEnvVariable("GENERATOR_NUM_DEVICES", "10")
+	numDevices, err := strconv.Atoi(numDevicesStr)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to read GENERATOR_NUM_DEVICES env variable: %s", err))
+	}
+
+	publishTimeoutMsStr := ReadEnvVariable("GENERATOR_PUBLISH_TIMEOUT_MS", "10")
+	publishTimeoutMs, err := strconv.Atoi(publishTimeoutMsStr)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to read GENERATOR_PUBLISH_TIMEOUT_MS env variable: %s", err))
+	}
+
+	wg := &sync.WaitGroup{}
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+
+	for i := 0; i < numDevices; i++ {
+		deviceID := fmt.Sprintf("%d", i)
+		mqttOptions := mqtt.NewClientOptions()
+		mqttOptions.AddBroker(brokerAddr)
+		publishTimeout := time.Duration(publishTimeoutMs) * time.Millisecond
+
+		device, err := NewDevice(deviceID, topic, publishTimeout, mqttOptions)
+		if err != nil {
+			fmt.Printf("Failed to connect device %s: %s\n", deviceID, err)
+			continue
+		}
+		fmt.Printf("Succesfully connected Device %s\n", deviceID)
+
+		wg.Add(1)
+		go device.PublishData(ctx, wg)
+		fmt.Printf("Device %s goroutine scheduled\n", deviceID)
+	}
+
+	keepRunning := true
+	for keepRunning {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Terminating: context cancelled")
+			keepRunning = false
+		case <-sigterm:
+			fmt.Println("Terminating: via signal")
+			keepRunning = false
+		}
+	}
+	cancel()
+	wg.Wait()
+
+	fmt.Println("### End of MQTT synthetic generation")
+}
+
+func ReadEnvVariable(env, fallback string) string {
+	v, ok := os.LookupEnv(env)
+	if !ok {
+		return fallback
+	}
+	return v
+}
