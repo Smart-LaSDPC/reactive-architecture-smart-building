0279cbc52974ec1f28f1182251913a482798b571
Gustavo Freire <gustavofreire@Gustavos-MacBook-Air.local>
2023-11-02 19:56:47 -0300

    add pgx and singleton instantiation

--- go-ingestor/go.mod
+++ go-ingestor/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/prometheus/client_golang v1.17.0
 	gopkg.in/yaml.v2 v2.4.0
+	github.com/jackc/pgx/v4 v4.10.1
 )
 
 require (
---
+++ go-ingestor/internal/db/postgres/client.go
@@ -0,0 +1,33 @@
+package postgres
+
+import (
+	"context"
+	_ "github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"log"
+	"sync"
+)
+
+var (
+	postgresClientPool *ClientPool
+	postgresOnce       sync.Once
+)
+
+type ClientPool struct {
+	Conn *pgxpool.Pool
+}
+
+func NewPostgresConnectionPool(dbHost string) *pgxpool.Pool {
+	postgresOnce.Do(func() {
+		config, err := pgxpool.ParseConfig(dbHost)
+		config.MinConns = 25
+		pool, err := pgxpool.ConnectConfig(context.Background(), config)
+		if err != nil {
+			log.Fatalf("Couldn't connect to the database. Reason %v", err)
+		}
+		pool.Stat()
+		postgresClientPool = &ClientPool{Conn: pool}
+	})
+	log.Printf("DB CONNECTION POOL CREATED")
+	return postgresClientPool.Conn
+}
